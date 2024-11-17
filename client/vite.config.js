9321aabb6fed6e2fac13db0eb67b485c7a6c5c60
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-16 19:07:59 -0500

    feat: add proxy configuration for API requests in Vite server setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fd39-104-158-139-32.ngrok-free.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});

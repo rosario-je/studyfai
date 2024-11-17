1b3129521c938cc2c5c857ba39ebe4d179fa7419
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-16 19:24:19 -0500

    feat: create user model schema for MongoDB with validation

---
+++ server/db/models/userModel.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkUserId: { type: String, unique: true, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', userSchema);;

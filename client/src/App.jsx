3c9de55c9b373e03e0b139b4d9b670adc196b160
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-20 00:18:56 -0500

    feat: implement routing for main application pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,25 @@
-import './App.css'
-import Landing from './Pages/Landing'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Pages import
+import Landing from "./Pages/Landing";
+import Dashboard from "./Pages/Dashboard";
+import Notes from "./Pages/Notes";
+import Quizzes from "./Pages/Quizzes";
+import Signin from "./Pages/Signin";
 
 function App() {
-
   return (
-    <>
-      <section>
-        <h2>This is going to be the studyfai application.</h2>
-        <Landing />
-      </section>
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/quizzes" element={<Quizzes />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;

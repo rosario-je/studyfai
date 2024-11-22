3df113c306838f199fbaf619c23dbce17c6cac31
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-21 21:55:50 -0500

    feat: integrate SectionA component into Landing page

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,71 +1,22 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { useState } from "react";
+// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+// import { useState } from "react";
 import Navbar from "../Components/Navbar";
+import SectionA from "../Components/Landing/SectionA";
 
 const Landing = () => {
-
-
   return (
     <div className="bg-white">
-      
       <Navbar />
-
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-center">
-            <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-              AI-powered studying for everyone
-            </h1>
-            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
-              Your ultimate study tool. Effortlessly take and enhance or summarize your notes with AI, then convert them into dynamic flashcards to master any subject. Transform your learning experience today!
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="btn-primary"
-              >
-                <h2 className="px-5 py-1">Get Started</h2>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-      </div>
+      <SectionA />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
 
-
-
-
-      // <SignedOut>
-      //   <SignInButton />
-      // </SignedOut>
-      // <SignedIn>
-      //   <UserButton />
-      // </SignedIn>
+// <SignedOut>
+//   <SignInButton />
+// </SignedOut>
+// <SignedIn>
+//   <UserButton />
+// </SignedIn>

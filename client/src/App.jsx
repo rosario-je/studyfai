a61e77583b477d23a3d9c24ba360c7f7a22b24f4
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-14 14:24:15 -0500

    feat: add signup route to the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./Pages/Dashboard";
 import Notes from "./Pages/Notes";
 import Quizzes from "./Pages/Quizzes";
 import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/notes" element={<Notes />} />
         <Route path="/quizzes" element={<Quizzes />} />

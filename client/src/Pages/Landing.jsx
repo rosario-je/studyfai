6caf7cb662c7e718c052698de759ad7a2412c913
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-14 14:24:40 -0500

    refactor: replace Navbar component import with Header component in Landing page

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,6 +1,6 @@
 // import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 // import { useState } from "react";
-import Navbar from "../Components/Navbar";
+import Navbar from "../Components/Header";
 import SectionA from "../Components/Landing/SectionA";
 import SectionB from "../Components/Landing/SectionB";
 import SectionC from "../Components/Landing/SectionC";

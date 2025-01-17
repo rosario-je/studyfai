6a528c746cbb41c0f2dc2f758b1cfe954f65d5a8
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-16 21:24:28 -0500

    feat: add MainLogo component for branding display

---
+++ client/src/components/ui/MainLogo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MainLogo = () => {
+    return (
+        <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+                <h1>
+                    Studyf<strong>AI</strong>
+                </h1>
+            </a>
+        </div>
+    )
+}
+
+export default MainLogo

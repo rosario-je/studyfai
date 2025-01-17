cd72c9635fcc1e96820bde5b2c8c5f34f8ddf12e
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-16 21:24:19 -0500

    feat: create DashboardNotes reusable component for displaying user notes

---
+++ client/src/components/ui/DashboardNotes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardNotes = () => {
+    return (
+        <div className='bg-zinc-100 hover:bg-zinc-200 hover:border-[1px] border-zinc-400 transition-colors w-[250px] h-[150px] flex-shrink-0 rounded-3xl p-6 items-center'>
+            <h2 className='text-zinc-500 leading-tight text-xl font-bold pb-2 truncate'>Linked List Notes</h2>
+            <p className='text-zinc-400 leading-tight text-base line-clamp-3'>These are the notes for the linked list notes that were summarized with studyFAI</p>
+        </div>
+    )
+}
+
+export default DashboardNotes

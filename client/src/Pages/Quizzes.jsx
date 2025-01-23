301e3b2424ec0419590275b096be7dd796d0a13a
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-23 00:39:33 -0500

    feat: integrate layout components in Quizzes page, adding Navbar and MainPageContainer for improved structure and user experience

--- client/src/Pages/Quizzes.jsx
+++ client/src/Pages/Quizzes.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
+//Layout components
+import MainPageContainer from '../components/layout/MainPageContainer.jsx'
+import RightContainer from '../components/layout/RightContainer.jsx'
+
+//Components import
+import Navbar from '../components/ui/Navbar.jsx'
+
 const Quizzes = () => {
+  const [expanded, setExpanded] = useState(true);
   return (
-    <section>
+    <>
       <SignedIn>
-        <div>Quizzes</div>
+        <Navbar />
+        <MainPageContainer expanded={expanded} setExpanded={setExpanded}>
+          <RightContainer expanded={expanded}>
+            <div>Quizzes</div>
+          </RightContainer>
+        </MainPageContainer>
       </SignedIn>
       <SignedOut>
         <RedirectToSignIn />
       </SignedOut>
-    </section>
+    </>
   );
 };
 

0054117796558c128cb2798b2f7405c4bf93b073
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-29 14:00:44 -0500

    style: update DashboardNotes component styles for improved appearance

--- client/src/components/ui/DashboardNotes.jsx
+++ client/src/components/ui/DashboardNotes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DashboardNotes = () => {
     return (
-        <div className='bg-zinc-100 hover:bg-zinc-200 hover:border-[1px] border-zinc-400 transition-colors w-[250px] h-[100%] flex-shrink-0 rounded-3xl p-6 items-center'>
+        <div className='bg-white hover:bg-gray-100 hover:border border-gray-200 transition-colors w-[250px] h-[100%] flex-shrink-0 rounded-3xl p-6 items-center'>
             <h2 className='text-zinc-500 leading-tight text-xl font-bold pb-2 truncate'>Linked List Notes</h2>
             <p className='text-zinc-400 leading-tight text-base line-clamp-3'>These are the notes for the linked list notes that were summarized with studyFAI</p>
         </div>

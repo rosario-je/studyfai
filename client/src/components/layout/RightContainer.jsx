bab800fad28d1051b971355a12ec17aba459b375
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-29 14:00:39 -0500

    feat: connect RightContainer component to Redux store for sidebar expansion state

--- client/src/components/layout/RightContainer.jsx
+++ client/src/components/layout/RightContainer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const RightContainer = ({ children, expanded }) => {
+//Redux imports
+import { useSelector } from 'react-redux';
+
+const RightContainer = ({ children }) => {
+    const expanded = useSelector((state) => state.sidebar.expanded)
     return (
         <section className={`main-container flex flex-col-reverse items-center w-full md:flex-row md:items-stretch justify-center gap-x-5 transition-all duration-200 px-6 h-full overflow-hidden bg-blue 
             ${expanded 

c36baf440b1a8d1638f11da8b7dbc2bf2368b3e4
Jose Eduardo Payamps <josenxs12@gmail.com>
2025-01-29 13:52:59 -0500

    refactor: simplify MainPageContainer by removing props and adjusting layout

--- client/src/components/layout/MainPageContainer.jsx
+++ client/src/components/layout/MainPageContainer.jsx
@@ -3,10 +3,10 @@ import { IoHomeOutline, IoBookOutline } from "react-icons/io5";
 import { MdOutlineQuiz } from "react-icons/md";
 
 import Sidebar, { SidebarItem } from '../ui/Sidebar.jsx'
-const MainPageContainer = ({ children, expanded, setExpanded }) => {
+const MainPageContainer = ({ children }) => {
     return (
-        <div className="dashboard-container h-[calc(100vh-85px)] flex mt-[85px] overflow-hidden">
-            <Sidebar expanded={expanded} setExpanded={setExpanded}>
+        <div className="dashboard-container h-[calc(100vh-85px)] flex mt-[80px] overflow-hidden bg-zinc-100">
+            <Sidebar>
                 <SidebarItem icon={<IoHomeOutline size={25} />} text="Home" />
                 <SidebarItem icon={<IoBookOutline size={25} />} text="Notes" />
                 <SidebarItem icon={<MdOutlineQuiz size={25} />} text="Quizzes" />
